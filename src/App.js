6d193585d5103512ca5180bd54239e2dd99ea2b3
Nayak <anuja.nayak@capgemini.com>
2023-05-31 13:57:35 +0530

    update alert

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [selectedName, setSelectedName] = useState('');
+
+  const handleChange = (event) => {
+    setSelectedName(event.target.value);
+  };
+
   return (
-    <div style={{ display: "flex", flexDirection: "column", gap: "15px",marginLeft:"5px" }}>
+    <div style={{ display: "flex", flexDirection: "column", gap: "15px", marginLeft: "5px" }}>
       <label>Enter Name</label>
-      <input type="text" style={{ width: "230px" }} />
+      <input type="text" value={selectedName} onChange={handleChange} style={{ width: "230px" }} />
       <button style={{ width: "240px" }}
-      onClick={() => {
-          alert("The name typed in textbox");
+        onClick={() => {
+          alert(selectedName);
         }}>Submit</button>
     </div>
   );
