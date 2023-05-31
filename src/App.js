0dcf323f9acce5955d7e161dd4b1ddcf75400896
Nayak <anuja.nayak@capgemini.com>
2023-05-31 15:33:33 +0530

    call the function

--- src/App.js
+++ src/App.js
@@ -7,15 +7,14 @@ function App() {
   const handleChange = (event) => {
     setSelectedName(event.target.value);
   };
-
+  const handleSubmit = () => {
+    alert(selectedName);
+  };
   return (
     <div style={{ display: "flex", flexDirection: "column", gap: "15px", marginLeft: "5px" }}>
       <label>Enter Name</label>
       <input type="text" value={selectedName} onChange={handleChange} style={{ width: "230px" }} />
-      <button style={{ width: "240px" }}
-        onClick={() => {
-          alert(selectedName);
-        }}>Submit</button>
+      <button style={{ width: "240px" }}onClick={handleSubmit}>Submit</button>
     </div>
   );
 }
